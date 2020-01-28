e9a6c5cf9d595b67d02e2fcd109e74b55a8b8eb5
Conor O'Reilly <conor@users-MacBook-Pro-245.local>
2020-01-28 23:25:24 +0000

    cleanup

--- _fyp/src/app/pages/tabs/tabs.page.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-page-tabs',
-  templateUrl: 'tabs.page.html',
-  styleUrls: ['tabs.page.scss']
-})
-export class TabsPage {}
