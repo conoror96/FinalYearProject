31e15d673d3fdef5c385b628afee68b55910967b
Conor O'Reilly <conor@users-MacBook-Pro-245.local>
2020-03-15 11:04:44 +0000

    apk successfully running on device. moving on to structure + nfc functions

--- main/src/environments/environment.prod.ts
+++ main/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDDusMHwYjCPVKz-UMGzt6AAYhxTKmj_PE",
+    authDomain: "accountdb-d624e.firebaseapp.com",
+    databaseURL: "https://accountdb-d624e.firebaseio.com",
+    projectId: "accountdb-d624e",
+    storageBucket: "accountdb-d624e.appspot.com",
+    messagingSenderId: "941414432769",
+    appId: "1:941414432769:web:a363f0b3161fe1746ac000",
+    measurementId: "G-B4NFVBF7NE"
+  }
 };
