b86b420109bb751b7681c5134cecbe1bb3202293
Conor O'Reilly <conor@users-MacBook-Pro-245.local>
2020-03-16 20:40:31 +0000

    deployed to firebase

--- main/functions/src/index.ts
+++ main/functions/src/index.ts
@@ -1,8 +1,28 @@
 import * as functions from 'firebase-functions';
-
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+import * as admin from 'firebase-admin';
+import Stripe from 'stripe';
+ 
+admin.initializeApp();
+ 
+const secret = functions.config().stripe.testkey;
+const stripe = new Stripe(secret, {
+  apiVersion: '2020-03-02',
+  typescript: true
+});
+ 
+export const createStripeCustomer = functions.auth.user().onCreate(async (snap, context) => {
+  console.log('snap: ', snap);
+  const customer = await stripe.customers.create({
+    email: snap.email
+  });
+ 
+  const batch = admin.firestore().batch();
+ 
+  const userRef = admin.firestore().collection('users').doc(snap.uid);
+  const cusRef = admin.firestore().collection('customers').doc(customer.id);
+ 
+  batch.update(userRef, { customerId: customer.id });
+  batch.set(cusRef, { user: snap.uid });
+ 
+  return batch.commit();
+});
