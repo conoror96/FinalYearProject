134347edfda810282a6610612f2a7507db414c2c
ChristianOlim <christianolim@hotmail.com>
2020-04-01 02:47:48 +0100

    Trying to get filter to work

--- main/src/app/pages/buyer-list/buyer-list.page.ts
+++ main/src/app/pages/buyer-list/buyer-list.page.ts
@@ -6,7 +6,7 @@ import { CartService } from '../../services/cart.service';
 import { ModalController } from '@ionic/angular';
 import { CartModalPage } from '../cart-modal/cart-modal.page';
 import { MenuController } from '@ionic/angular';
-
+import { AngularFirestore } from '@angular/fire/firestore';
 
 
 @Component({
@@ -18,15 +18,51 @@ export class BuyerListPage implements OnInit {
   products: Observable<any>;
   cartItemCount: BehaviorSubject<number> = this.cartService.getCartItemCount();
 
+  //public products: any[];
+  //public loadedProducts: any[];
+
   constructor(private auth: AuthService, private productService: ProductService, 
     private cartService: CartService, private modalCtrl: ModalController,
-    private menu: MenuController) { }
+    private menu: MenuController, private firestore: AngularFirestore) { }
     
 
   ngOnInit() {
     this.products = this.productService.getAllProducts();
     
+    //this.goalList = goalList
+
+    /*this.firestore.collection(`products`).valueChanges()
+      .subscribe(products => {
+        this.products = products;
+        this.loadedProducts = products;
+    }); */
   }
+
+  /*
+  initializeItems(): void {
+    this.products = this.loadedProducts;
+  }
+
+  filterProducts(event) {
+    this.initializeItems();
+  
+    const searchTerm = event.srcElement.value;
+  
+    if (!searchTerm) {
+      return;
+    }
+  
+    this.products = this.products.filter(currentGoal => {
+      if (currentGoal.goalName && searchTerm) {
+        if (currentGoal.goalName.toLowerCase().indexOf(searchTerm.toLowerCase()) > -1) {
+          return true;
+        }
+        return false;
+      }
+    });
+  }
+  */
+
 
   signOut() {
     this.auth.signOut();
